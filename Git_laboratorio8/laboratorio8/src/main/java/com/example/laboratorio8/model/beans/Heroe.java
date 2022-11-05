99c1c35301dbf3e359069b2feab49847eee51f9f
Gustavo <a20192858@pucp.edu.pe>
2022-11-05 00:36:53 -0500

    heroesVerLista

---
+++ Git_laboratorio8/laboratorio8/src/main/java/com/example/laboratorio8/model/beans/Heroe.java
@@ -0,0 +1,86 @@
+package com.example.laboratorio8.model.beans;
+
+public class Heroe {
+    private int heroeId;
+    private String nombre;
+    private int edad;
+    private String genero;
+    private String clase;
+    private int puntos_experiencia;
+    private int ataque;
+    private int nivel_inicial;
+    private int pareja_id;
+
+    public int getHeroeId() {
+        return heroeId;
+    }
+
+    public void setHeroeId(int heroeId) {
+        this.heroeId = heroeId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public int getPuntos_experiencia() {
+        return puntos_experiencia;
+    }
+
+    public void setPuntos_experiencia(int puntos_experiencia) {
+        this.puntos_experiencia = puntos_experiencia;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getNivel_inicial() {
+        return nivel_inicial;
+    }
+
+    public void setNivel_inicial(int nivel_inicial) {
+        this.nivel_inicial = nivel_inicial;
+    }
+
+    public int getPareja_id() {
+        return pareja_id;
+    }
+
+    public void setPareja_id(int pareja_id) {
+        this.pareja_id = pareja_id;
+    }
+}
+

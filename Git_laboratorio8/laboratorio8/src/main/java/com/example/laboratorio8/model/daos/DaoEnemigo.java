4dbdb5935bbf4051a1466f52613208076c1d5c90
Leonardo <a20201696@pucp.edu.pe>
2022-11-07 05:28:15 -0500

    DaoEnemigo

--- Git_laboratorio8/laboratorio8/src/main/java/com/example/laboratorio8/model/daos/DaoEnemigo.java
+++ Git_laboratorio8/laboratorio8/src/main/java/com/example/laboratorio8/model/daos/DaoEnemigo.java
@@ -14,7 +14,18 @@ public class DaoEnemigo {
         }
         String url = "jdbc:mysql://localhost:3306/mydb";
         ArrayList<Enemigo> lista = new ArrayList<>();
-        String sql = "select * from enemigos";
+        String sql = "SELECT *, \n" +
+                "case when genero = \"M\" then \"masculino\"\n" +
+                "when genero = \"F\" then \"femenino\" \n" +
+                "when genero = \"O\" then \"otros\"\n" +
+                "else \"-\"\n" +
+                "end as genero_completo\n" +
+                "FROM mydb.enemigos\n" +
+                "INNER JOIN mydb.catalogo_objetos\n" +
+                "ON enemigos.objeto_id = catalogo_objetos.idobjeto\n" +
+                "INNER JOIN mydb.submenu_clases\n" +
+                "ON enemigos.clase_id = submenu_clases.clase_idclase\n" +
+                ";";
         try (Connection connection = DriverManager.getConnection(url, "root", "123456");
              Statement stmt = connection.createStatement();
              ResultSet rs = stmt.executeQuery(sql);) {
@@ -26,9 +37,9 @@ public class DaoEnemigo {
                 enemigo.setAtaque(rs.getInt(3));
                 enemigo.setExperienciaEntregada(rs.getInt(4));
                 enemigo.setProbabilidadTirarObjeto(rs.getInt(5));
-                enemigo.setObjeto_entregado(rs.getString(6));
-                enemigo.setGenero(rs.getString(8));
-                enemigo.setClase(rs.getString(9));
+                enemigo.setGenero(rs.getString("genero"));
+                enemigo.setObjeto_entregado(rs.getString(7));
+                enemigo.setClase(rs.getString(8));
                 lista.add(enemigo);
             }
         } catch (SQLException e) {
@@ -36,4 +47,67 @@ public class DaoEnemigo {
         }
         return lista;
     }
+    public Enemigo submenu(String idEnemigo) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String sql = "SELECT *, \n" +
+                "case when genero = \"M\" then \"masculino\"\n" +
+                "when genero = \"F\" then \"femenino\" \n" +
+                "when genero = \"O\" then \"otros\"\n" +
+                "else \"-\"\n" +
+                "end as genero_completo\n" +
+                "FROM mydb.enemigos\n" +
+                "INNER JOIN mydb.catalogo_objetos\n" +
+                "ON enemigos.objeto_id = catalogo_objetos.idobjeto\n" +
+                "INNER JOIN mydb.submenu_clases\n" +
+                "ON enemigos.clase_id = submenu_clases.clase_idclase\n" +
+                ";";
+        Enemigo enemigo = null;
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, idEnemigo);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    enemigo = new Enemigo();
+
+                    enemigo.setEnemigoId(rs.getInt(1));
+                    enemigo.setNombreEnemigo(rs.getString(2));
+                    enemigo.setAtaque(rs.getInt(3));
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+
+        return enemigo;
+    }
+    public void borrar(String idEnemigo){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String sql = "DELETE from mydb.enemigos WHERE idEnemigos= ?";
+
+        try(Connection connection = DriverManager.getConnection(url,"root","root");
+            PreparedStatement pstmt=connection.prepareStatement(sql))
+        {
+
+            pstmt.setString(1,idEnemigo);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
